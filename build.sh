2c039c1cd3b0bb7f213387d01ec98dcacd6d9788
Akshatchourey <achourey09@gmail.com>
2025-11-02 00:57:34 +0530

    specified python verson in general

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ npm install
 npx tailwindcss -i ./dashboard/static/dashboard/css/input.css -o ./dashboard/static/dashboard/css/min-tailwind.css --minify
 
 # step3: installing requirements
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Step 4: Collect static files for Django
 python manage.py collectstatic --no-input
